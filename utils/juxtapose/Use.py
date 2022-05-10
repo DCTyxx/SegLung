477b427c586bf8e5a7e89514a6e8a74c8dafdc71
DCTyxx <39491638+DCTyxx@users.noreply.github.com>
2022-05-10 21:04:13 +0800

    solve the bug about superview

--- utils/juxtapose/Use.py
+++ utils/juxtapose/Use.py
@@ -1,61 +1,64 @@
 import streamlit.components.v1 as components
+import cv2 as cv
+from PIL import Image
+import pathlib
+import streamlit as st
 
-def juxtapose(img1: str, img2: str, height: int = 1000):  # data
-
-    """Create a new timeline component.
-    Parameters
-    ----------
-    height: int or None
-        Height of the timeline in px
-    Returns
-    -------
-    static_component: Boolean
-        Returns a static component with a timeline
-    """
-
-    # load css + js
+def superView(raw_image,segmented_img,IMG1,IMG2,height: int = 800):
     cdn_path = "https://cdn.knightlab.com/libs/juxtapose/latest"
     css_block = f'<link rel="stylesheet" href="{cdn_path}/css/juxtapose.css">'
     js_block = f'<script src="{cdn_path}/js/juxtapose.min.js"></script>'
+
+
+    STREAMLIT_STATIC_PATH = (
+        pathlib.Path(st.__path__[0]) / "static"
+    )
+    Image1 = Image.fromarray(cv.cvtColor(raw_image, cv.COLOR_GRAY2RGB))
+    Image2 = Image.fromarray(cv.cvtColor(segmented_img, cv.COLOR_BGR2RGB))
+    Image1.save(STREAMLIT_STATIC_PATH / IMG1)
+    Image2.save(STREAMLIT_STATIC_PATH / IMG2)
+
+
 
     # write html block
     htmlcode = (
         css_block
         + """ 
-    """
+        """
         + js_block
         + """
-        <div id="foo" style="width: 95%; height: """
+            <div id="foo" style="width: 95%; height: """
         + str(height)
         + '''px; margin: 1px;"></div>
-        <script>
-        slider = new juxtapose.JXSlider('#foo',
-            [
-                {
-                    src: "'''
-        + img1
+            <script>
+            slider = new juxtapose.JXSlider('#foo',
+                [
+                    {
+                        src: "'''
+        + IMG1
         + '''",
-                    label: 'img1',
-                },
-                {
-                    src: "'''
-        + img2
+                        label: 'Raw',
+                    },
+                    {
+                        src: "'''
+        + IMG2
         + """",
-                    label: 'img2',
-                }
-            ],
-            {
-                animate: true,
-                showLabels: true,
-                showCredits: true,
-                startingPosition: "50%",
-                makeResponsive: true
-            });
-        </script>
-    """
+                        label: 'Segmentation',
+                    }
+                ],
+                {
+                    animate: true,
+                    showLabels: true,
+                    showCredits: true,
+                    startingPosition: "50%",
+                    makeResponsive: true
+                });
+            </script>
+        """
     )
     static_component = components.html(
         htmlcode,
         height=height,
     )
+
     return static_component
