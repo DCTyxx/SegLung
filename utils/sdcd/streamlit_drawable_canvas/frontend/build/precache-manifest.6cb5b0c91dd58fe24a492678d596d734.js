a366bd35e8eb3c1af6cd8bf8200d8d22400200d6
DCTyxx <39491638+DCTyxx@users.noreply.github.com>
2022-05-15 21:38:10 +0800

    maybe save the bug about fixSegt.py

---
+++ utils/sdcd/streamlit_drawable_canvas/frontend/build/precache-manifest.6cb5b0c91dd58fe24a492678d596d734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d968a4384bbc4c1fa92acfefa8006ff6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1ded32c4664d93db86c",
+    "url": "./static/css/main.4c0f3d3b.chunk.css"
+  },
+  {
+    "revision": "7802dfd8e4312dae17b3",
+    "url": "./static/js/2.5ffbf28f.chunk.js"
+  },
+  {
+    "revision": "7d4080a6d0300c83db1045a99ffdd499",
+    "url": "./static/js/2.5ffbf28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ded32c4664d93db86c",
+    "url": "./static/js/main.1cd317aa.chunk.js"
+  },
+  {
+    "revision": "77e61c67eb4f95e15891",
+    "url": "./static/js/runtime-main.ff285346.js"
+  }
+]);
